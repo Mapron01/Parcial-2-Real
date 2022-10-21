f7053baa27955453bda0dc5311b841f4960e746d
Mapron01 <111908643+Mapron01@users.noreply.github.com>
2022-10-21 14:25:59 -0500

    Synchronizing job ParcialB3 from AWS Glue at 2022/10/21 19:25:59

--- ParcialB3/ParcialB2.py
+++ ParcialB3/ParcialB2.py
@@ -14,24 +14,12 @@ x2 = now.strftime("%m")
 x3 = now.strftime("%d")
 
 s3 = boto3.client('s3')
-data = s3.download_file('parcial2primer','headlines/raw/ElTiempo/year='+ x + '/month=' + x2 + '/day=' + x3 + '/archivo1.txt', '/tmp/archivo1.txt')
+data = s3.download_file('parcial2primer','headlines/final/ElTiempo/year='+ x + '/month=' + x2 + '/day=' + x3 + '/procesado1.csv', '/tmp/archivo1.csv')
 
-data2 = s3.download_file('parcial2primer','headlines/raw/Espectador/year='+ x + '/month=' + x2 + '/day=' + x3 + '/archivo2.txt', '/tmp/archivo2.txt')
-
-with open("/tmp/archivo1.txt") as fp:
-    soup = BeautifulSoup(fp, 'html.parser')
-
-Title1 = soup.find_all(class_="title page-link")
-Category1 = soup.find_all(class_="category-published")
-Link1 = soup.find_all(class_="multimediatag page-link")
-
-
-s = ""
-for i in range(len(Title1)):
-    s = s + Title1[i].contents[0] + "\n"
+data2 = s3.download_file('parcial2primer','headlines/final/Espectador/year='+ x + '/month=' + x2 + '/day=' + x3 + '/procesado2.csv', '/tmp/archivo2.csv')
 
 
 client = boto3.client("s3","us-east-1")
-client.put_object(Body=s, Bucket='parcial2primer', Key ='headlines/final/ElTiempo/year='+ x + '/month=' + x2 + '/day=' + x3 + 'procesado1.csv')
+client.put_object(Body=/tmp/archivo1.csv, Bucket='parcial2primer', Key ='news/raw/ElTiempo/year='+ x + '/month=' + x2 + '/day=' + x3 + '/Final1.csv')
 
-#client.put_object(Body=data2.data.decode('utf-8'), Bucket='parcial2primer', Key ='headlines/final/Espectador/year='+ x + '/month=' + x2 + '/day=' + x3 + 'procesado1.csv')
+client.put_object(Body=/tmp/archivo2.csv, Bucket='parcial2primer', Key ='news/raw/Espectador/year='+ x + '/month=' + x2 + '/day=' + x3 + '/Final2.csv')
